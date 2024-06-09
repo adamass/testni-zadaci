24c05f5544458e53ec4aff147bea56d1ba45f481
adamass <43586138+adamass@users.noreply.github.com>
2024-06-09 16:39:30 +0200

    Initial commit.

---
+++ Task1/index.js
@@ -0,0 +1,29 @@
+const extraParagraph = document.getElementsByClassName('card__extra_paragraph')[0];
+const btn = document.getElementsByClassName('btn')[0];
+const hiddenElement = document.querySelectorAll('.card');
+
+
+function showText() {
+
+    btn.classList.toggle("fade-in");
+
+    extraParagraph.classList.toggle('extra_paragraph');
+} 
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else
+            {
+                entry.target.classList.remove('show');
+            }
+    });
+});
+
+hiddenElement.forEach((element) => observer.observe(element));
+
+
+
