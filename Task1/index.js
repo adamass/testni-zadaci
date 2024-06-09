51a3d591529fe65dbf5de7ee5857e478fbade385
adamass <43586138+adamass@users.noreply.github.com>
2024-06-09 21:30:29 +0200

    Bonus Task.

--- Task1/index.js
+++ Task1/index.js
@@ -1,29 +1,24 @@
-const extraParagraph = document.getElementsByClassName('card__extra_paragraph')[0];
-const btn = document.getElementsByClassName('btn')[0];
-const hiddenElement = document.querySelectorAll('.card');
-
+const extraParagraph = document.getElementsByClassName(
+  "card__extra_paragraph"
+)[0];
+const btn = document.getElementsByClassName("btn")[0];
+const hiddenElement = document.querySelectorAll(".card");
 
 function showText() {
+  btn.classList.toggle("fade-in");
 
-    btn.classList.toggle("fade-in");
-
-    extraParagraph.classList.toggle('extra_paragraph');
-} 
+  extraParagraph.classList.toggle("extra_paragraph");
+}
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        // console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        else
-            {
-                entry.target.classList.remove('show');
-            }
-    });
+  entries.forEach((entry) => {
+    // console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
 hiddenElement.forEach((element) => observer.observe(element));
-
-
-
